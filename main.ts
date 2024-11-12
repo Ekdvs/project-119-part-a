8b046e06856877e014cdb817b571b974c8ac207a
Ekdvs <ekdvsampath02@gmail.com>
2024-11-12 18:50:22 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    music.play(music.builtinPlayableSoundEffect(soundExpression.twinkle), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+})
